11dcb5911803a286d1fd9b5fd258ff5a42b170c7
levitatingmoon <94081701+levitatingmoon@users.noreply.github.com>
2024-06-22 15:35:29 +0200

    Changed starting colours

--- Assets/Script/GlobalVar.cs
+++ Assets/Script/GlobalVar.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public static class GlobalVar
 {
-    public static Color color1 = new Color(87.0f / 255, 77.0f / 255, 104.0f / 255);
-    public static Color color2 = new Color(242.0f / 255, 232.0f / 255, 109.0f / 255);
+    public static Color color1 = new Color(121.0f / 255, 42.0f / 255, 255.0f / 255);
+    public static Color color2 = new Color(255.0f / 255, 236.0f / 255, 0.0f / 255);
     public static int coins = 3000;
 
     //score based on lines made and comboMultiplier
